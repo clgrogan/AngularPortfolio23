21aeee33d1b2f255e3980c898687cb775aca473e
Curt Grogan <curt.grogan@fdmgroup.com>
2023-10-12 11:22:52 -0400

    Added looping on COURSES to projects view.

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { AppModule } from '../app.module';
 import { ProjectCardComponent } from '../project-card/project-card.component';
+import { PROJECTS } from '../data/db-projects';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss'],
-  imports: [ProjectCardComponent],
+  imports: [ProjectCardComponent, CommonModule],
   standalone: true
 })
 export class ProjectsComponent {
+
+  projects = PROJECTS;
 
 }
