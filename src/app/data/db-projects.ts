707bcba2de3c7502401235113c72714036517392
Curt Grogan <curt.grogan@fdmgroup.com>
2023-11-06 11:31:14 -0500

    Updated project images to use this app's assets

--- src/app/data/db-projects.ts
+++ src/app/data/db-projects.ts
@@ -2,7 +2,7 @@ export const PROJECTS: any = [
   {
     id: 1,
     description: "React App Consuming YouTube API",
-    thumbUrl: "https://curt-g-fcc-portfolio.netlify.app/images/dog-search-project.png",
+    thumbUrl: "../assets/images/dog-search-project.png",
     altText: "Animated screenshots of full-stack application UI.",
     longDescription: "Front-end React / Javascript application which consumes the YouTube Rest API. Displays a list of dog videos and adds an option to add to the search.",
     projectUrl: "https://search-dogs.netlify.app",
@@ -12,7 +12,7 @@ export const PROJECTS: any = [
   {
     id: 2,
     description: "Full Stack React / C# .NET / PostgreSQL",
-    thumbUrl: "https://curt-g-fcc-portfolio.netlify.app/images/project1WalkThrough.gif",
+    thumbUrl: "../assets/images/project1WalkThrough.gif",
     altText: "Photo of computer and phone with application on the screens.",
     longDescription: "Mobile-First Shopping List App ⬧ (aka: Honey Get List) Full-Stack App. Front-End (Client-side) responsive SPA application using HTML, CSS, JavaScript (JS), React and Axios for API. Back-End (Server-side) using .NET, C#, EF Core with an SQL database. Deployed to Netlify using yarn and Heroku utilizing Docker.",
     projectUrl: "https://honey-get-client-cgrogan.netlify.app",
@@ -22,7 +22,7 @@ export const PROJECTS: any = [
   {
     id: 3,
     description: "Angular App Consumes GitHub API",
-    thumbUrl: "https://curt-g-fcc-portfolio.netlify.app/images/project4.jpg",
+    thumbUrl: "../assets/images/project4.jpg",
     altText: "Stock photo showing a monitor, keyboard, tablet, and mouse.",
     longDescription: "Angular application which consumes the GitHub Restful API. Displays a list of my followers.",
     projectUrl: "https://github-followers-angular-project.netlify.app",
@@ -32,7 +32,8 @@ export const PROJECTS: any = [
   {
     id: 4,
     description: "Vanilla (HTML/CSS) Portfolio",
-    thumbUrl: "https://curt-g-fcc-portfolio.netlify.app/images/VanillaPortfolioAnimated.gif",
+    // thumbUrl: "../assets/images/VanillaPortfolioAnimated.gif",
+    thumbUrl: "../assets/images/VanillaPortfolioAnimated.gif",
     altText: "Animated screenshots of the Vanilla Portfolio",
     longDescription: "This project is built using HTML5, and SCSS/SASS (CSS3).",
     projectUrl: "https://curtg.netlify.com/",
@@ -43,7 +44,7 @@ export const PROJECTS: any = [
   {
     id: 5,
     description: "Spring Boot Rest API",
-    thumbUrl: "https://curt-g-fcc-portfolio.netlify.app/images/spring-api-logo-bg.png",
+    thumbUrl: "../assets/images/spring-api-logo-bg.png",
     altText: "Spring and Rest API Logos",
     longDescription: "Built using Maven, Spring Boot, Spring Security, Hibernate, and Java to serve a restful API.",
     projectUrl: "https://github.com/clgrogan/SurveyRestAPI/tree/main/src/main/java/com/restapi",
